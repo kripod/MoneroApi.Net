08f1f0d09a28aaeabe93faa4dda8331a8a35c2bb
Jojatekok <jojatekokweb@gmail.com>
2015-02-02 18:40:41 +0100

    Added a note to the demo's config file

--- MoneroApi.Net.Demo/Config.cs
+++ MoneroApi.Net.Demo/Config.cs
@@ -4,7 +4,9 @@ namespace Jojatekok.MoneroAPI.Demo
 {
     struct Config
     {
-        // This file's changes should not be tracked by git
+        // This file's changes should not be tracked by git.
+        // In order to disable tracking, execute the following command in your shell:
+        // git update-index --assume-unchanged "MoneroApi.Net.Demo/Config.cs"
 
         public static readonly IRpcSettings ClientRpcSettings = new RpcSettings();
         public static readonly IPathSettings ClientPathSettings = new PathSettings();
