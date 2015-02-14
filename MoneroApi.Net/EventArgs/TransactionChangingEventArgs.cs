bf3392051a4437bf41ccfbc7d8b2a1c0ba11ec69
Jojatekok <jojatekokweb@gmail.com>
2015-02-14 13:45:04 +0100

    Fixed a property conflict

--- MoneroApi.Net/EventArgs/TransactionChangingEventArgs.cs
+++ MoneroApi.Net/EventArgs/TransactionChangingEventArgs.cs
@@ -2,15 +2,10 @@
 {
     public class TransactionChangingEventArgs : ValueChangingEventArgs<Transaction>
     {
-        public Transaction NewValue { get; private set; }
-        public Transaction OldValue { get; private set; }
-
         public int TransactionIndex { get; private set; }
 
         internal TransactionChangingEventArgs(Transaction newValue, Transaction oldValue, int transactionIndex) : base(newValue, oldValue)
         {
-            NewValue = newValue;
-            OldValue = oldValue;
             TransactionIndex = transactionIndex;
         }
     }
